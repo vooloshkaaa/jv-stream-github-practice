80e92adaa81a4bef45435754c3e0e14c3fd8ccae
Hanna Voloshko <aa.voloshko09@gmail.com>
2025-09-25 23:37:50 +0300

    all tasks are done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,32 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final int YEARS_OF_RESIDENCE = 10;
+    @Override
+    public boolean test(Candidate c) {
+        if (c.getAge() < 35) {
+            return false;
+        }
+        if (!c.isAllowedToVote()) {
+            return false;
+        }
+        if (!"Ukrainian".equals(c.getNationality())) {
+            return false;
+        }
+        return hasMinimumResidence(c, YEARS_OF_RESIDENCE);
+    }
+
+    private boolean hasMinimumResidence(Candidate c, int yearsOfResidence) {
+        return Stream.of(c.getPeriodsInUkr().split(","))
+                .map(String::trim)
+                .mapToInt(range -> {
+                    String[] years = range.split("-");
+                    return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+                })
+                .sum() >= yearsOfResidence;
+    }
 }
