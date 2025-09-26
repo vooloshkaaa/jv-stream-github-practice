4eb8c4ba4581aa25432b046409e4b7226b52354e
Hanna Voloshko <aa.voloshko09@gmail.com>
2025-09-26 18:24:38 +0300

    all issues are fixed

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,8 +1,10 @@
 package practice;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import model.Candidate;
@@ -85,7 +87,8 @@ public class StreamPractice {
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
         return peopleList.stream()
                 .filter(p -> p.getSex() == Person.Sex.WOMAN && p.getAge() >= femaleAge)
-                .flatMap(p -> p.getCats().stream())
+                .flatMap(p -> Optional.ofNullable(p.getCats())
+                        .orElse(Collections.emptyList()).stream())
                 .map(Cat::getName)
                 .collect(Collectors.toList());
     }
